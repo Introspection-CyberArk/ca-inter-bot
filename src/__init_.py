709737406a0a421c845b14f91ae91f25a2bf1e8d
Introspection-CyberArk <angel32.oliver@gmail.com>
2026-07-26 17:40:42 +0530

    Create __init_.py

---
+++ src/__init_.py
@@ -0,0 +1,10 @@
+"""
+CA Intermediate Exam Bot
+Created by: MeNgHeaNg
+Powered by: @Introspection007
+Version: 2.0
+"""
+
+__version__ = "2.0"
+__author__ = "MeNgHeaNg"
+__powered_by__ = "@Introspection007"
